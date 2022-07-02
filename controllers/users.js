aea67e7a8bfe56e0000e827d0f3631decb5c7041
Tatyana Chernyshova <tadrugova@yandex.ru>
2022-07-03 04:11:16 +0800

    fix: коррект юзера

--- controllers/users.js
+++ controllers/users.js
@@ -53,12 +53,7 @@ module.exports.createUser = (req, res, next) => {
     .then((hash) => User.create({
       name, email, password: hash,
     }))
-    .then((user) => res.status(201).send({
-      data: {
-        name: user.name,
-        email: user.email,
-      },
-    }))
+    .then((user) => res.status(201).send({ user }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         next(new ValidationError('Для создания пользователя отправлены некорректные данные.'));
